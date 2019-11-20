0008c65afe75436fa36aaa4b3bb2de0596c63604
erickgarces30 <erick.garces@pucese.edu.ec>
2019-11-20 12:57:44 -0500

    Cambio de billetes

--- AppPruebaFactorial/Program.cs
+++ AppPruebaFactorial/Program.cs
@@ -13,8 +13,26 @@ namespace AppPruebaFactorial
             int num = 5;
             double res = AppProgramacionCapas.Factorial.fact(num);
             Console.WriteLine("El factorial es: {0} es: {1} ", num, res );
+
+            cambio(27.50);
             Console.ReadKey(); //Pausa
 
         }
+
+        static void cambio(double dolares)
+        {
+            int billete10 = (int)dolares / 10;
+            Console.WriteLine("{0} billetes de 10 dólares", billete10);
+
+            int billete5 = ((int)dolares % 10) / 5;
+            Console.WriteLine("{0} billetes de 5 dólares", billete5);
+
+            int billete1 = (((int)dolares % 10) % 5) / 1;
+            Console.WriteLine("{0} billetes de 1 dólar", billete1);
+
+            int moneda50 = (((int)dolares % 10) % 5) % 1)/0.50;
+            Console.WriteLine("{0} monedas de 50 centavos", moneda50);
+        }
+
     }
 }
